8d383b31718fcb06313b67c29a8a4bcffe3cd487
S X V X G E <54964470+Sxvxgee@users.noreply.github.com>
2022-10-18 22:39:01 +0400

    emojis and embeds used in commands now

--- DiscordBot/cogs/downloads.py
+++ DiscordBot/cogs/downloads.py
@@ -1,6 +1,6 @@
-import time, random, os
+import time, random, shutil, os
 
-from discord import app_commands, Interaction, File
+from discord import app_commands, Interaction, File, Embed, InteractionMessage
 from discord.ext.commands import Bot, Cog
 from typing import Literal
 from internal.useful import download_video, download_mp3, mp3_to_8d
@@ -14,75 +14,113 @@ class Downloads(Cog):
   # TODO: Check if filesize is over 8mb for non-boosted servers
   # TODO: If server is boosted, get level and do math accordingly
   # TODO: Use a file uploading service I guess
-  # TODO: Make an embed that shows status with animated emojis on different parts of the command, such as "downloading", "converting", etc
 
   @app_commands.command(name = "mp4", description = "Download videos in MP4 format.")
   @app_commands.describe(url = "The URL of the video you'd like to download, from YouTube.", resolution = "The desired resolution of the video.")
   async def mp4_download(self, interaction : Interaction, url : str, resolution : Literal['720p', '480p', '360p', '240p', '144p'] = '720p'):
     start_time = time.time()
-    await interaction.response.defer(ephemeral = True, thinking = True)
+
+    embed = Embed(
+      title = "Downloading video...",
+      description = f"<:pending:1031984015680475277> Video is being downloaded...",
+      color = 0x000001
+    )
+    
+    await interaction.response.send_message(embed = embed)
     path = await download_video(interaction, url, resolution)
 
     if path is False:
-      return await interaction.followup.send(f"Invalid video URL.")
+      embed.title = "Failed to download video"
+      embed.description = "<:failed:1031982289212682332> Invalid video URL."
+      return await interaction.edit_original_response(embed = embed)
     if path is None:
-      return await interaction.followup.send(f"Couldn't find a video with a matching URL.")
-
+      embed.title = "Failed to download video"
+      embed.description = "<:failed:1031982289212682332> Couldn't find a video with a matching URL."
+      return await interaction.edit_original_response(embed = embed)
+    
     try:
-      await interaction.followup.send(f"Finished downloading the video in {round(time.time() - start_time)}s", file = File(path))
+      embed.title = "Video downloaded"
+      embed.description = f"<:checkmark:1031982292601688144> Video downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(time.time() - start_time)}s"
+      await interaction.edit_original_response(embed = embed, attachments = File(path))
     except:
-      await interaction.followup.send(f"Finished downloading the video in {round(time.time() - start_time)}s. However the file size was too big.")
+      embed.title = "Fatal error"
+      embed.description = f"<:checkmark:1031982292601688144> Video downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(time.time() - start_time)}s\n<:failed:1031982289212682332> Failed to upload video due to Discord's upload size limit ({round(os.path.getsize(path)/125000)}MB)"
+      await interaction.edit_original_response(embed = embed)
 
-    os.remove(path)
-    # TODO: Check if filesize is over 8mb for non-boosted servers
-    # TODO: If server is boosted, get level and do math accordingly
-    # TODO: Use a file uploading service I guess
+    shutil.rmtree(f"/home/sxvxge/Desktop/YouTubeTools/downloads/{interaction.user.id}")
 
   @app_commands.command(name = "mp3", description = "Download videos in MP3 format.")
   @app_commands.describe(url = "The URL of the video you'd like to download, from YouTube.")
   async def mp3_download(self, interaction : Interaction, url : str):
     start_time = time.time()
-    await interaction.response.defer(ephemeral = False, thinking = True)
+
+    embed = Embed(
+      title = "Downloading audio...",
+      description = f"<:pending:1031984015680475277> Audio is being downloaded...",
+      color = 0x000001
+    )
+
+    await interaction.response.send_message(embed = embed)
     path = await download_mp3(interaction, url)
 
     if path is False:
-      return await interaction.followup.send(f"Invalid video URL.")
+      embed.title = "Failed to download audio"
+      embed.description = "<:failed:1031982289212682332> Invalid video URL."
+      return await interaction.edit_original_response(embed = embed)
     if path is None:
-      return await interaction.followup.send(f"Couldn't find a video with a matching URL.")
+      embed.title = "Failed to download audio"
+      embed.description = "<:failed:1031982289212682332> Couldn't find a video with a matching URL."
+      return await interaction.edit_original_response(embed = embed)
 
     try:
-      await interaction.followup.send(f"Finished downloading the video in {round(time.time() - start_time)}s", file = File(path, f"{random.randint(0, 6969)}-{interaction.user.name}.mp3"))
+      embed.title = "Audio downloaded"
+      embed.description = f"<:checkmark:1031982292601688144> Audio downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(time.time() - start_time)}s"
+      await interaction.edit_original_response(embed = embed, attachments = File(path))
     except:
-      await interaction.followup.send(f"Finished downloading the video in {round(time.time() - start_time)}s. However the file size was too big.")
+      embed.title = "Fatal error"
+      embed.description = f"<:checkmark:1031982292601688144> Audio downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(time.time() - start_time)}s\n<:failed:1031982289212682332> Failed to upload audio due to Discord's upload size limit ({round(os.path.getsize(path)/125000)}MB)"
+      await interaction.edit_original_response(embed = embed)
 
-    os.remove(path)
-    # TODO: Check if filesize is over 8mb for non-boosted servers
-    # TODO: If server is boosted, get level and do math accordingly
-    # TODO: Use a file uploading service I guess
-
+    shutil.rmtree(f"/home/sxvxge/Desktop/YouTubeTools/downloads/{interaction.user.id}")
 
   @app_commands.command(name = "8d", description = "Download videos in MP3 format and convert them into 8D.")
-  @app_commands.describe(url = "The URL of the video you'd like to download, from YouTube.")
-  async def youtube_to_8d(self, interaction : Interaction, url : str):
+  @app_commands.describe(url = "The URL of the video you'd like to download, from YouTube.", period = "The period for 8D switching between both sides.")
+  async def youtube_to_8d(self, interaction : Interaction, url : str, period : int):
     start_time = time.time()
-    await interaction.response.defer(ephemeral = False, thinking = True)
+    
+    embed = Embed(
+      title = "Downloading audio...",
+      description = f"<:pending:1031984015680475277> Audio is being downloaded...\n<:havent_started:1031982290911383682> Convert to 8D",
+      color = 0x000001
+    )
+    await interaction.response.send_message(embed = embed)
     path = await download_mp3(interaction, url)
 
     if path is False:
-      return await interaction.followup.send(f"Invalid video URL.")
+      embed.title = "Failed to download audio"
+      embed.description = "<:failed:1031982289212682332> Invalid video URL."
+      return await interaction.edit_original_response(embed = embed)
     if path is None:
-      return await interaction.followup.send(f"Couldn't find a video with a matching URL.")
-
-    converted_video_path = await mp3_to_8d(path, path)
+      embed.title = "Failed to download audio"
+      embed.description = "<:failed:1031982289212682332> Couldn't find a video with a matching URL."
+      return await interaction.edit_original_response(embed = embed)
+    
+    conversion_time = time.time()
+    embed.title = "Converting audio..."
+    embed.description = f"<:checkmark:1031982292601688144> Audio downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(conversion_time - start_time)}s\n<:pending:1031984015680475277> Converting audio to 8D..."
+    await interaction.edit_original_response(embed = embed)
+    converted_video_path = await mp3_to_8d(path, path, period)
 
     try:
-      await interaction.followup.send(f"Finished downloading the video in {round(time.time() - start_time)}s", file = File(converted_video_path, f"{random.randint(0, 6969)}-{interaction.user.name}.mp3"))
+      embed.title = "Audio downloaded"
+      embed.description = f"<:checkmark:1031982292601688144> Audio downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(conversion_time - start_time)}s\n<:time:1031983535743049809> Conversion time: {round(time.time() - conversion_time)}s\n<:checkmark:1031982292601688144> Audio converted to 8D successfully."
+      await interaction.edit_original_response(embed = embed, attachments = File(path))
     except:
-      await interaction.followup.send(f"Finished downloading the video in {round(time.time() - start_time)}s. However the file size was too big.")
+      embed.title = "Fatal error"
+      embed.description = f"<:checkmark:1031982292601688144> Audio downloaded successfully.\n<:time:1031983535743049809> Downloading time: {round(conversion_time - start_time)}s\n<:time:1031983535743049809> Conversion time: {round(time.time() - conversion_time)}s\n<:failed:1031982289212682332> Failed to upload audio due to Discord's upload size limit ({round(os.path.getsize(path)/125000)}MB)"
+      await interaction.edit_original_response(embed = embed)
 
-    os.remove(path)
-    if converted_video_path != path:
-      os.remove(converted_video_path)
+    shutil.rmtree(f"/home/sxvxge/Desktop/YouTubeTools/downloads/{interaction.user.id}")
 
 
 async def setup(bot : Bot):
